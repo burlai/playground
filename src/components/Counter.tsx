b1cfbc1c4f54764fd0461b4300f445761930682c
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2023-07-16 13:30:03 +0200

    virtual DOM element

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,41 @@
 import React, { useState } from "react";
 
 interface CounterInterface {
-    initialCount: number,
+  initialCount: number;
 }
 
-const Counter: React.FC<CounterInterface> = ({initialCount}) => {
+const virtualDOMElement = {
+  type: "div",
+  props: {
+    children: [
+      {
+        type: "div",
+        props: {
+          className: "mb-2",
+          children: ["Count: ", "count"],
+        },
+      },
+      {
+        type: "button",
+        props: {
+          className: "btn btn-primary d-inline-block me-2",
+          onClick: "increment",
+          children: "Increment",
+        },
+      },
+      {
+        type: "button",
+        props: {
+          className: "btn btn-secondary d-inline-block",
+          onClick: "decrement",
+          children: "Decrement",
+        },
+      },
+    ],
+  },
+};
+
+const Counter: React.FC<CounterInterface> = ({ initialCount }) => {
   const [count, setCount] = useState(initialCount);
 
   const increment = () => {
@@ -20,8 +51,15 @@ const Counter: React.FC<CounterInterface> = ({initialCount}) => {
   return (
     <div>
       <div className="mb-2">Count: {count}</div>
-      <button className="btn btn-primary d-inline-block me-2" onClick={increment}>Increment</button>
-      <button className="btn btn-secondary d-inline-block" onClick={decrement}>Decrement</button>
+      <button
+        className="btn btn-primary d-inline-block me-2"
+        onClick={increment}
+      >
+        Increment
+      </button>
+      <button className="btn btn-secondary d-inline-block" onClick={decrement}>
+        Decrement
+      </button>
     </div>
   );
 };
